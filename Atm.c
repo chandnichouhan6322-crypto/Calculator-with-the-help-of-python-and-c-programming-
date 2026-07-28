c3a589a290d7475f3b2f3e417e737563091ed5bf
chandnichouhan6322-crypto <chandnichouhan6322@gmail.com>
2026-07-28 22:37:23 +0530

    Add files via upload

---
+++ Atm.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main(){
+		float bal=1000;
+		float deposit;
+		float withdraw;
+		int choice=0;
+		while(choice!=4){
+			printf("-----------ATM----------\n");
+			printf("1 Check balance \n 2 Deposite\n 3 Withdrawl \n 4 exit \n");
+			printf("enter a choice");
+		    scanf("%d",&choice);
+		    
+			if (choice==1){
+				printf("okh! you want to check your balance");
+				printf("\n your balance is----%.2f",bal);}
+				else if(choice==2){
+					printf("okh! you want to deposite money");
+					printf("\nenter your deposit");
+					scanf("%f",&deposit);
+					bal=bal+deposit;
+					printf("you deposited---%.2f",deposit);
+					printf("\nNow your total deposit is----%.2f",bal);}
+				else if(choice==3){
+					printf("okh! you want to withdrew money");
+					printf("\nenter withdrawl amt");
+					scanf("%f",&withdraw);
+					
+					if(withdraw<=bal){
+					
+					    printf("\nwithdrawl done");
+					    bal=bal-withdraw;
+					    }
+					  else{
+					  	printf("insufficient bal");}
+					printf("new balance%.2f",bal);
+				}
+				else if( choice==4){
+					printf("okh! you want to exist");
+				}
+				else{
+					printf("invalid choice");
+				}}
+		
+			return 0;
+		}
