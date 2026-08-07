155cebadf6a640ce89ce95e20dbf15f09b5b7e2f
chandnichouhan6322-crypto <chandnichouhan6322@gmail.com>
2026-08-07 22:36:54 +0530

    Add files via upload

---
+++ star_pattern.c
@@ -0,0 +1,18 @@
+//*
+//* *
+//* * *
+//* * * * 
+//* * * * *
+
+#include<stdio.h>
+int main(){
+	int k,l,m=5;
+	for(k=1;k<=m;k++){
+		for(l=1;l<=k;l++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+		
